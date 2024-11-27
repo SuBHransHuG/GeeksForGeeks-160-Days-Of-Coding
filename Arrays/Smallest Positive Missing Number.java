a7d750ddf4271bc9bcfc9c8d4d462b0334db3aac
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-11-27 22:57:56 +0530

    Smallest Positive Missing Number

---
+++ Arrays/Smallest Positive Missing Number.java
@@ -0,0 +1,28 @@
+class Solution {
+    // Function to find the smallest positive number missing from the array.
+    public int missingNumber(int[] arr) {
+        // Your code here
+         int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= 0 || arr[i] > n) {
+                arr[i] = n + 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(arr[i]);
+            if (num <= n) {
+                arr[num - 1] = -Math.abs(arr[num - 1]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
