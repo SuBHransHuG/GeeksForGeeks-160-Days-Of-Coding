0c143afba6bf93c175cbfa3b4469db7de646b6d0
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-12-24 17:32:28 +0530

    Search in a sorted Matrix

---
+++ Matrix/Search in a sorted Matrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+            for (int i = 0; i < mat.length; i++) {
+            if (Arrays.binarySearch(mat[i], x) >= 0) { 
+                return true;
+            }
+        }
+        return false; 
+    }
+}
