689f7436460c070c0a0a9717924a0d7441afab34
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2025-01-03 10:45:45 +0530

    Count Subarrays with given XOR

---
+++ Hashing/Count Subarrays with given XOR.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSubarraysWithXOR(int[] arr, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int count = 0;
+        int pXor = 0;
+        hm.put(0, 1);
+
+        for (int i : arr) {
+            pXor ^= i; 
+
+            int rXor = pXor ^ k;
+            if (hm.containsKey(rXor)) {
+                count += hm.get(rXor);
+            }
+            hm.put(pXor, hm.getOrDefault(pXor, 0) + 1);
+        }
+
+        return count;
+    }
+}
